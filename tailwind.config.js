2a7568502b06cad4c89ac57a078df82900ee0e3c
antoninhrlt <antoninhrlt1@gmail.com>
2022-06-07 20:16:15 +0200

    Use tailwindcss with Laravel

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    colors: {
+      'dark': '#0d1117',
+      'white': '#ffffff',
+      'lightgray': '#f5f5f5',
+      'blue': '#094f62'
+    },
+    fontFamily: {
+      sans: ['Verdana', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
