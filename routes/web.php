7694ccfdc12ebed10bbdc498bd82a3bcc351eddc
antoninhrlt <antoninhrlt1@gmail.com>
2022-06-08 22:28:37 +0200

    New routes for new views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
